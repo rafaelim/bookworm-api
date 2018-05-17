0cfca692a04844aebb65b5bc56d93b14e6ea3085
Rafael Lima <rafael.lima@univem.edu.br>
2018-05-16 22:38:35 -0300

    user signup

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import path from 'path';
 import mongoose from 'mongoose';
 import auth from './routes/auth';
+import users from './routes/users';
 import bodyParse from 'body-parser';
 import dotenv from 'dotenv';
 import Promise from 'bluebird';
@@ -12,7 +13,10 @@ app.use(bodyParse.json());
 mongoose.Promise = Promise;
 mongoose.connect(process.env.MONGODB_URL)
 import bcrypt from 'bcrypt-nodejs';
+
 app.use('/api/auth', auth);
+app.use('/api/users', users);
+
 app.get('/*', (req,res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
 })
